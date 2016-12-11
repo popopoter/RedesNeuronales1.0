fe2b80c621ff7c0b0dd0e4a8bb163335293bb6ac
danielwikstrom <danielwikstrom22@hotmail.com>
2016-12-12 00:29:44 +0100

    backpropagation funcional

--- src/redNeuronal/RedNeuronal.java
+++ src/redNeuronal/RedNeuronal.java
@@ -10,7 +10,8 @@ public class RedNeuronal {
 	//public ArrayList <Double> input;
 	//public ArrayList <Double> output;
 	
-	
+	public ArrayList <Double> salidas;
+	public ArrayList<Double> peso;
 	
 	
 	public RedNeuronal(int nEntradas, int nSalidas){
@@ -76,20 +77,148 @@ public class RedNeuronal {
 	}
 
 	public void forwardPropagation(ArrayList<Double> input){
-		
+		salidas = new ArrayList<Double>();
+		peso = new ArrayList<Double>();
 	ArrayList <Double> aux = new ArrayList<Double>();
+	int j=0;
+	
 		for(Capa capa: capas){
+			int i=0;
 			
 			for(Neurona ne: capa.neuronas){
+				for(int k=0; k< ne.pesos.size();k++)
+				{
+					peso.add(ne.pesos.get(k));	
+				}
 				aux.add(ne.activar(input));
+				
+					salidas.add(aux.get(i));
+					
+				
+				
+				i++;
+				j++;
 			}
 			input.clear();
 			input.addAll(aux);
 			aux.clear();
 			
 		}
+	
 		
 		}
+	public void BackPropagation(ArrayList<Double> input){
 		
-	}
+		//objetivo que se busca
+		double target=0.5;
+		
+		int parada=0;//contador para la parada de prueba
+		//salida de la red neuronal
+		double salidaO;
+		double in1= input.get(0);
+		double in2= input.get(1);
+		
+		ArrayList<Double> aux = new ArrayList<Double>();
+		aux.add(in1);
+		aux.add(in2);
+		
+		//variacion de pesos de las entradas a cada una de las neuronas de la capa oculta
+		ArrayList<Double> varIH= new ArrayList<Double>();
+		for(int i=0; i<10;i++)
+		{
+			varIH.add(0.0);
+		}
+	
+		
+		//variacion de pesos de las neuronas ocultas a la capa de salida
+		ArrayList<Double> varHO= new ArrayList<Double>();
+		for(int i=0; i<5;i++)
+		{
+			varHO.add(0.0);
+		}
+	
+		
+		do{
+			ArrayList<Double> aux2 = new ArrayList<Double>();
+			aux2.add(in1);
+			aux2.add(in2);
+			
+			//error de la salida de las neuronas de la capa oculta
+			ArrayList<Double> errorH= new ArrayList<Double>();
+			for(int i=0; i<5;i++)
+			{
+				errorH.add(0.0);
+			}
+			
+			
+			//error de la salida 
+			double errorO;
+			
+			//sumatorio del error de la salida por los pesos de cada uno de los pesos de la capa oculta
+			double sumatorio=0;
+			
+			//constante de aprendizaje
+			double aprendizaje=0.5;
+			
+			forwardPropagation(aux2);
+			salidaO=aux.get(0);
+			errorO= salidaO*(1-salidaO)*(target-salidaO);
+			for(int i=0; i<varHO.size() ;i++)
+			{
+				varHO.set(i,varHO.get(i) + aprendizaje*errorO*salidas.get(i));
+			}
 
+			for(int i=0; i<5;i++){
+				sumatorio += errorO*peso.get(i+10);
+			}
+			
+			for(int i=0; i<errorH.size();i++)
+			{
+				errorH.set(i, salidas.get(i)*(1-salidas.get(i))*sumatorio);
+			}
+
+		
+
+			for(int i=0; i<varIH.size();i++)
+			{
+					if((i%2)==0){
+						if(i>=5)
+						varIH.set(i,varIH.get(i)+aprendizaje*errorH.get(i-5)*aux.get(0));
+						else
+							varIH.set(i,varIH.get(i)+aprendizaje*errorH.get(i)*aux.get(0));
+					}
+					else{
+						if(i>=5)
+							varIH.set(i,varIH.get(i)+aprendizaje*errorH.get(i-5)*aux.get(1));
+							else
+								varIH.set(i,varIH.get(i)+aprendizaje*errorH.get(i)*aux.get(1));
+					}	
+			}
+			
+		
+
+			System.out.println("pesos antes de cambiar: ");
+			for(int i =0; i<peso.size();i++)
+				System.out.println(peso.get(i));
+			
+			for(int i=0; i<peso.size();i++){
+				if(i<10)
+					peso.set(i, peso.get(i)+varIH.get(i));
+				else
+					peso.set(i, peso.get(i)+varHO.get(i-10));
+
+			}
+			System.out.println("pesos despues de cambiar: ");
+			for(int i =0; i<peso.size();i++)
+				System.out.println(peso.get(i));
+			aux2.clear();
+			
+			
+			parada++;
+			//actualizar la lista de pesos con sus nuevos valores
+		}while(parada<5);//condicion de parada para probar
+
+
+	}
+	
+	}
--- src/redNeuronal/main.java
+++ src/redNeuronal/main.java
@@ -8,11 +8,11 @@ public class main {
 		
 		RedNeuronal red= new RedNeuronal(2,1);
 		ArrayList<Double> input = new ArrayList<Double>();
-		System.out.println(new Sigmoidal().activar(-5));
-		input.add((double) 0);
-		input.add((double) 8);
-		red.forwardPropagation(input);
-		
+		//System.out.println("hola " + new Sigmoidal().activar(-5));
+		input.add((double) -5);
+		input.add((double) 1);
+		red.BackPropagation(input);
+		System.out.println("salida: ");
 		for(double d: input){
 			System.out.println(d);
 		}
